c548b592f2271cda45977a61d0cb6f7633c89cfc
yockii <xuyuqi@gmail.com>
2023-10-26 15:34:45 +0800

    feat: 优化common结构

--- pkg/database/Model.go
+++ pkg/database/Model.go
@@ -1,7 +1,5 @@
 package database
 
-import "github.com/yockii/qscore/pkg/util"
-
 type Model interface {
 	TableComment() string
 	AddRequired() string
@@ -18,55 +16,3 @@ type Model interface {
 }
 
 var Models []Model
-
-type BaseModel struct {
-	ID uint64 `json:"id,omitempty,string" gorm:"primaryKey;autoIncrement:false"`
-}
-
-func (*BaseModel) TableComment() string {
-	return "空表"
-}
-func (m *BaseModel) AddRequired() string {
-	return ""
-}
-func (m *BaseModel) InstanceRequired() string {
-	if m.ID == 0 {
-		return "id"
-	}
-	return ""
-}
-func (m *BaseModel) CheckDuplicatedModel() Model {
-	return nil
-}
-func (m *BaseModel) UpdateConditionModel() Model {
-	b := new(BaseModel)
-	b.ID = m.ID
-	return b
-}
-func (m *BaseModel) UpdateModel() Model {
-	panic("implement me")
-}
-func (m *BaseModel) InitDefaultFields() {
-	m.ID = util.SnowflakeId()
-}
-func (m *BaseModel) UpdateRequired() string {
-	if m.ID == 0 {
-		return "id"
-	}
-	return ""
-}
-func (m *BaseModel) DeleteRequired() string {
-	if m.ID == 0 {
-		return "id"
-	}
-	return ""
-}
-func (m *BaseModel) FuzzyQueryMap() map[string]string {
-	return nil
-}
-func (m *BaseModel) ExactMatchModel() Model {
-	return m
-}
-func (m *BaseModel) ListOmits() string {
-	return ""
-}
